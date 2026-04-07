eebb18e1c6ff189b4a6020391740e0e7cb1fcfde
Vishwaraj Singh Shekhawat <vishwaraj.singh1523@gmail.com>
2026-04-07 20:53:27 +0530

    Add initial HTML structure for the frontend application

--- day8/backend/src/app.js
+++ day8/backend/src/app.js
@@ -1,12 +1,12 @@
 const express = require("express")
 const notesModel = require("./model/note.model")
 const cors = require("cors")
-
+const path = require("path")
 const app = express()
 
 app.use(cors())
 app.use(express.json())
-
+app.use(express.static("./public"))
 /* post */
 app.post("/api/notes", async (req, res) => {
     const { title, description } = req.body
@@ -49,6 +49,8 @@ app.patch('/api/notes/:id', async (req, res) => {
         message: "note updated successfully"
     })
 })
-
+app.use('*name', (req, res) => {
+    res.sendFile(path.join(__dirname, "..","/public/index.html"))
+})
 
 module.exports = app
--- day8/frontend/src/App.jsx
+++ day8/frontend/src/App.jsx
@@ -1,43 +1,99 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 function App() {
-  const [notes, setnotes] = useState([
-    {
-    title: "test title 1",
-    description: "test description 1"
-    },
-    {
-    title: "test title 2",
-    description: "test description 2"
-    },
-    {
-    title: "test title 3",
-    description: "test description 3"
-    },
-    {
-    title: "test title 4",
-    description: "test description 4"
-    },
-  ])
+  const [notes, setnotes] = useState([])
 
-  axios.get("http://localhost:3000/api/notes")
-    .then((res) => {
-      console.log(res.data)
-  })
+  function fetchNotes() {
+    axios.get("http://localhost:3000/api/notes")
+      .then((res) => {
+        setnotes(res.data.notes)
+      })
+  }
+
+  useEffect(() => {
+    fetchNotes()
+  }, [])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    const formData = new FormData(e.target)
+    const title = formData.get('title')?.toString().trim()
+    const description = formData.get('description')?.toString().trim()
+
+    // console.log(title, description)
+
+    axios.post("http://localhost:3000/api/notes", {
+      title: title,
+      description: description
+    })
+      .then((res) => {
+        // console.log(res.data)
+        fetchNotes()
+        e.target.reset()
+      })
+  }
+
+  function handleDelete(noteId) {
+    // console.log(noteId)
+    axios.delete(`http://localhost:3000/api/notes/${noteId}`)
+      .then(res => {
+
+        fetchNotes()
+      })
+  }
+
+  function handleResubmit(e) {
+    e.preventDefault()
+    const formData = new FormData(e.target)
+    const noteId = formData.get('noteId')?.toString().trim()
+    const description = formData.get('description')?.toString().trim()
+
+    let ntId = notes[noteId - 1]._id
+
+    axios.patch(`http://localhost:3000/api/notes/${ntId}`, {
+      description: description
+    })
+      .then(res => {
+        fetchNotes()
+        e.target.reset()
+    })
+  }
+
   return (
     <>
+
+      <div className="forms">
+        <form className='note-create-form' onSubmit={handleSubmit}>
+          <h2>Add Note</h2>
+          <input type="text" name="title" placeholder='Enter title' />
+          <input type="text" name="description" placeholder="Enter description" />
+          <button type="submit">Create Note</button>
+        </form>
+
+        <form className='note-create-form' onSubmit={handleResubmit}>
+          <h2>Update description</h2>
+          <input type="text" name="noteId" placeholder='Enter note number' />
+          <input type="text" name="description" placeholder="Enter modified description" />
+          <button type="submit">update Note</button>
+        </form>
+      </div>
+
       <div className="notes">
         {
           notes.map(note => {
-            return <div className="note">
+            return <div className="note" key={note._id || note.id}>
               <h2>{note.title}</h2>
               <p>{note.description}</p>
-              </div>
+              <button onClick={() => { handleDelete(note._id) }}>Delete</button>
+            </div>
           })
         }
-        
+
       </div>
+
+      
     </>
   )
 }
