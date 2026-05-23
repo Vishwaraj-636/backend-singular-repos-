555ab80b2e701ef290a67fac1983c7f21fe8c84d
Vishwaraj Singh Shekhawat <vishwaraj.singh1523@gmail.com>
2026-05-24 02:01:18 +0530

    moodify-d1

---
+++ day-14/backend/src/config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+function connectToDB(){
+  mongoose.connect(process.env.MONGO_URI)
+  .then(()=>{
+    console.log("connected to DB");
+  })
+  .catch((e)=>{
+    console.log("Error connecting to DB: ",e)
+  })
+}
+
+module.exports = connectToDB;
---
+++ day-14/frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+import FaceExpression from './features/expression/component/FaceExpression';
+const App = () => {
+  return (
+    <FaceExpression />
+  );
+};
+
+export default App;
